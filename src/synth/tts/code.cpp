ae0d4f93416bb0dc8873b1a110dfffd5c5fa8053
iLambda <me@ilambda.io>
2020-05-20 23:10:01 +0200

    Added basic code support

---
+++ src/synth/tts/code.cpp
@@ -0,0 +1,318 @@
+#include <mbed.h>
+#include <rtos.h>
+
+#include "code.h"
+
+/* The table of legible names for a command */
+const char* const tts_cmd_instruction[] = {
+    "P0", "P10", "P20", "P70",
+    "W3", "W6", "W9", 
+    "FA", "SL", 
+    "STS", "RLX", 
+    "R", "D", "\x07P",
+    "(", ")"
+};
+/* The table of legible names for a phoneme */
+const char* const tts_phoneme_instruction[] = {
+    "IY", "IH", "EY", "EH", "AY", 
+    "AX", "UX", "OH", "AW", "OW", 
+    "UH", "UW", "MM", "NE", "NO", 
+    "NGE", "NGO", "LE", "LO", "WW", 
+    "RR", "IYRR", "EYRR", "AXRR", "AWRR", 
+    "OWRR", "EYIY", "OHIY", "OWIY", "OHIH", 
+    "IYEH", "EHLL", "IYUW", "AXUW", "IHWW", 
+    "AYWW", "OWWW", "JH", "VV", "ZZ", 
+    "ZH", "DH", "BE", "BO", "EB", 
+    "OB", "DE", "DO", "ED", "OD", 
+    "GE", "GO", "EG", "OG", "CH", 
+    "HE", "HO", "WH", "FF", "SE", 
+    "SO", "SH", "TH", "TT", "TU", 
+    "TS", "KE", "KO", "EK", "OK", 
+    "PE", "PO", "R0", "R1", "R2", 
+    "R3", "R4", "R5", "R6", "R7", 
+    "R8", "R9", "A0", "A1", "A2", 
+    "A3", "A4", "A5", "A6", "A7", 
+    "A8", "A9", "B0", "B1", "B2", 
+    "B3", "B4", "B5", "B6", "B7", 
+    "B8", "B9", "C0", "C1", "C2", 
+    "C3", "C4", "C5", "C6", "C7", 
+    "C8", "C9", "D0", "D1", "D2", 
+    "D3", "D4", "D5", "D6", "D7", 
+    "D8", "D9", "D10", "D11", "M0", 
+    "M1", "M2", "--"
+ };
+/* The table of legible names for an invalid value */
+const char* const tts_invalid_instruction = "??";
+
+/* The table of legible names for a command */
+const char* const tts_cmd_name[] = {
+    "Pause 0ms", "Pause 100ms", "Pause 200ms", "Pause 700ms",
+    "Wait 30ms", "Wait 60ms", "Wait 90ms", 
+    "Fast", "Slow", 
+    "Stress", "Relax", 
+    "Repeat for", "Delay for", "Pitch",
+    "Begin group", "End group"
+};
+/* The table of legible names for a phoneme */
+const char tts_phoneme_long_vowel[] = "Long vowel";
+const char tts_phoneme_short_vowel[] = "Short vowel";
+const char tts_phoneme_nasal[] = "Nasal";
+const char tts_phoneme_resonate[] = "Resonate";
+const char tts_phoneme_color_vowel[] = "Color vowel";
+const char tts_phoneme_diphtong[] = "Diphthong";
+const char tts_phoneme_affricative[] = "Affricate";
+const char tts_phoneme_fictive[] = "Fictive";
+const char tts_phoneme_stop[] = "Voiced stop";
+const char tts_phoneme_fricative[] = "Fricative";
+const char tts_phoneme_robot[] = "Robot";
+const char tts_phoneme_alarm[] = "Alarm";
+const char tts_phoneme_beeps[] = "Beeps";
+const char tts_phoneme_biological[] = "Biological";
+const char tts_phoneme_dtmf[] = "DTMF";
+const char tts_phoneme_misc[] = "Misc";
+const char tts_phoneme_end[] = "End of word";
+const char* const tts_phoneme_name[] = {
+    tts_phoneme_long_vowel,
+    tts_phoneme_short_vowel,
+    tts_phoneme_long_vowel,
+    tts_phoneme_short_vowel,
+    tts_phoneme_short_vowel,
+    tts_phoneme_short_vowel,
+    tts_phoneme_short_vowel,
+    tts_phoneme_short_vowel,
+    tts_phoneme_short_vowel,
+    tts_phoneme_long_vowel,
+    tts_phoneme_short_vowel,
+    tts_phoneme_long_vowel,
+    tts_phoneme_nasal,
+    tts_phoneme_nasal,
+    tts_phoneme_nasal,
+    tts_phoneme_nasal,
+    tts_phoneme_nasal,
+    tts_phoneme_resonate,
+    tts_phoneme_resonate,
+    tts_phoneme_resonate,
+    tts_phoneme_resonate,
+    tts_phoneme_color_vowel,
+    tts_phoneme_color_vowel,
+    tts_phoneme_color_vowel,
+    tts_phoneme_color_vowel,
+    tts_phoneme_color_vowel,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_diphtong,
+    tts_phoneme_affricative,
+    tts_phoneme_fictive,
+    tts_phoneme_fictive,
+    tts_phoneme_fictive,
+    tts_phoneme_fictive,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_affricative,
+    tts_phoneme_fricative,
+    tts_phoneme_fricative,
+    tts_phoneme_fricative,
+    tts_phoneme_fricative,
+    tts_phoneme_fricative,
+    tts_phoneme_fricative,
+    tts_phoneme_fricative,
+    tts_phoneme_fricative,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_stop,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_robot,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_alarm,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_beeps,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_biological,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_dtmf,
+    tts_phoneme_misc,
+    tts_phoneme_misc,
+    tts_phoneme_misc,
+    tts_phoneme_end
+ };
+
+/* Check if value is a command or a phoneme */
+synth::tts_code_type_t synth::tts_code_type(const uint8_t& code) {
+    /* Check */
+    if (code & 0x80) {
+        /* Phonemes are values from 128 to 254 */
+        return TTS_TYPE_PHONEME;
+    } else if (code <= TTS_CMD_LAST) {
+        /* A command must be castable to the above enum */
+        return TTS_TYPE_COMMAND;
+    } else {
+        /* Invalid */
+        return TTS_TYPE_INVALID;
+    }
+}
+
+/* Go to next value in given range, for a given code */
+void synth::tts_code_next(uint8_t& code) {
+    /* Depending on its type, */
+    switch (tts_code_type(code)) {
+        /* If command */
+        case TTS_TYPE_COMMAND: {
+            /* Handle rollover if necessary */
+            if (code == TTS_CMD_LAST) {
+                code = TTS_CMD_FIRST;
+            } else {
+                ++code;
+            }
+            break;
+        }
+
+        /* If phoneme */
+        case TTS_TYPE_PHONEME: {
+            /* Increment and rollover */
+            ++code;
+            code = 0x80 | ((code) & 0x7F);
+            break;
+        }
+
+        /* If invalid, bring back to phoneme type, and set to an end character */
+        default: {
+            code = TTS_PHON_END;
+            break;
+        }
+        
+    }
+}
+
+/* Go to prev value in given range, for a given code */
+void synth::tts_code_prev(uint8_t& code) {
+    /* Depending on its type, */
+    switch (tts_code_type(code)) {
+        /* If command */
+        case TTS_TYPE_COMMAND: {
+            /* Handle rollover if necessary */
+            if (code == TTS_CMD_FIRST) {
+                code = TTS_CMD_LAST;
+            } else {
+                --code;
+            }
+            break;
+        }
+
+        /* If phoneme */
+        case TTS_TYPE_PHONEME: {
+            /* Increcrement and rollover */
+            --code;
+            code = 0x80 | (code & 0x7F);
+            break;
+        }
+
+        /* If invalid, bring back to phoneme type, and set to an end character */
+        default: {
+            code = TTS_PHON_END;
+            break;
+        }
+        
+    }
+}
+
+/* Return the name of a code */
+const char* synth::tts_code_instruction(const uint8_t& code) {
+    /* Depending on its type, */
+    switch (tts_code_type(code)) {
+        /* If command */
+        case TTS_TYPE_COMMAND:
+            return tts_cmd_instruction[code];
+
+        /* If phoneme */
+        case TTS_TYPE_PHONEME:
+            return tts_phoneme_instruction[code & 0x7F];
+
+        /* If invalid, bring back to phoneme type, and set to an end character */
+        default:
+            return tts_invalid_instruction;            
+    }
+}
+
+/* Return the name of a code */
+const char* synth::tts_code_name(const uint8_t& code) {
+    /* Depending on its type, */
+    switch (tts_code_type(code)) {
+        /* If command */
+        case TTS_TYPE_COMMAND:
+            return tts_cmd_name[code];
+
+        /* If phoneme */
+        case TTS_TYPE_PHONEME:
+            return tts_phoneme_name[code & 0x7F];
+
+        /* If invalid, bring back to phoneme type, and set to an end character */
+        default:
+            return nullptr;        
+    }
+}
---
+++ src/synth/tts/code.h
@@ -0,0 +1,191 @@
+#ifndef __SING_N_SPELL_SYNTH_CODE
+#define __SING_N_SPELL_SYNTH_CODE
+
+#include <mbed.h>
+#include <rtos.h>
+
+#ifdef BUILD_CONFIG_DEBUG
+    #include <type_traits>
+#endif
+
+#define TTS_CMD_FIRST           synth::tts_cmd_t::TTS_CMD_PAUSE0
+#define TTS_CMD_LAST            synth::tts_cmd_t::TTS_CMD_GROUP_END
+
+namespace synth {
+
+    /* A tts command */
+    enum tts_cmd_t : uint8_t {
+        TTS_CMD_PAUSE0,
+        TTS_CMD_PAUSE100,
+        TTS_CMD_PAUSE200,
+        TTS_CMD_PAUSE700,
+        TTS_CMD_WAIT30,
+        TTS_CMD_WAIT60,
+        TTS_CMD_WAIT90, 
+        TTS_CMD_FAST,
+        TTS_CMD_SLOW,
+        TTS_CMD_STRESS,
+        TTS_CMD_RELAX,
+        TTS_CMD_REPEAT,
+        TTS_CMD_DELAY_10MS,
+        TTS_CMD_PITCH,
+        TTS_CMD_GROUP_OPEN,
+        TTS_CMD_GROUP_END
+    };
+    /* A tts phoneme */
+    enum tts_phoneme_t : uint8_t {
+        TTS_PHON_IY = 128, 
+        TTS_PHON_IH = 129, 
+        TTS_PHON_EY = 130, 
+        TTS_PHON_EH = 131, 
+        TTS_PHON_AY = 132, 
+        TTS_PHON_AX = 133, 
+        TTS_PHON_UX = 134, 
+        TTS_PHON_OH = 135, 
+        TTS_PHON_AW = 136, 
+        TTS_PHON_OW = 137, 
+        TTS_PHON_UH = 138, 
+        TTS_PHON_UW = 139, 
+        TTS_PHON_MM = 140, 
+        TTS_PHON_NE = 141, 
+        TTS_PHON_NO = 142, 
+        TTS_PHON_NGE = 143, 
+        TTS_PHON_NGO = 144, 
+        TTS_PHON_LE = 145, 
+        TTS_PHON_LO = 146, 
+        TTS_PHON_WW = 147, 
+        TTS_PHON_RR = 148, 
+        TTS_PHON_IYRR = 149, 
+        TTS_PHON_EYRR = 150, 
+        TTS_PHON_AXRR = 151, 
+        TTS_PHON_AWRR = 152, 
+        TTS_PHON_OWRR = 153, 
+        TTS_PHON_EYIY = 154, 
+        TTS_PHON_OHIY = 155, 
+        TTS_PHON_OWIY = 156, 
+        TTS_PHON_OHIH = 157, 
+        TTS_PHON_IYEH = 158, 
+        TTS_PHON_EHLL = 159, 
+        TTS_PHON_IYUW = 160, 
+        TTS_PHON_AXUW = 161, 
+        TTS_PHON_IHWW = 162, 
+        TTS_PHON_AYWW = 163, 
+        TTS_PHON_OWWW = 164, 
+        TTS_PHON_JH = 165, 
+        TTS_PHON_VV = 166, 
+        TTS_PHON_ZZ = 167, 
+        TTS_PHON_ZH = 168, 
+        TTS_PHON_DH = 169, 
+        TTS_PHON_BE = 170, 
+        TTS_PHON_BO = 171, 
+        TTS_PHON_EB = 172, 
+        TTS_PHON_OB = 173, 
+        TTS_PHON_DE = 174, 
+        TTS_PHON_DO = 175, 
+        TTS_PHON_ED = 176, 
+        TTS_PHON_OD = 177, 
+        TTS_PHON_GE = 178, 
+        TTS_PHON_GO = 179, 
+        TTS_PHON_EG = 180, 
+        TTS_PHON_OG = 181, 
+        TTS_PHON_CH = 182, 
+        TTS_PHON_HE = 183, 
+        TTS_PHON_HO = 184, 
+        TTS_PHON_WH = 185, 
+        TTS_PHON_FF = 186, 
+        TTS_PHON_SE = 187, 
+        TTS_PHON_SO = 188, 
+        TTS_PHON_SH = 189, 
+        TTS_PHON_TH = 190, 
+        TTS_PHON_TT = 191, 
+        TTS_PHON_TU = 192, 
+        TTS_PHON_TS = 193, 
+        TTS_PHON_KE = 194, 
+        TTS_PHON_KO = 195, 
+        TTS_PHON_EK = 196, 
+        TTS_PHON_OK = 197, 
+        TTS_PHON_PE = 198, 
+        TTS_PHON_PO = 199, 
+        TTS_PHON_R0 = 200, 
+        TTS_PHON_R1 = 201, 
+        TTS_PHON_R2 = 202, 
+        TTS_PHON_R3 = 203, 
+        TTS_PHON_R4 = 204, 
+        TTS_PHON_R5 = 205, 
+        TTS_PHON_R6 = 206, 
+        TTS_PHON_R7 = 207, 
+        TTS_PHON_R8 = 208, 
+        TTS_PHON_R9 = 209, 
+        TTS_PHON_A0 = 210, 
+        TTS_PHON_A1 = 211, 
+        TTS_PHON_A2 = 212, 
+        TTS_PHON_A3 = 213, 
+        TTS_PHON_A4 = 214, 
+        TTS_PHON_A5 = 215, 
+        TTS_PHON_A6 = 216, 
+        TTS_PHON_A7 = 217, 
+        TTS_PHON_A8 = 218, 
+        TTS_PHON_A9 = 219, 
+        TTS_PHON_B0 = 220, 
+        TTS_PHON_B1 = 221, 
+        TTS_PHON_B2 = 222, 
+        TTS_PHON_B3 = 223, 
+        TTS_PHON_B4 = 224, 
+        TTS_PHON_B5 = 225, 
+        TTS_PHON_B6 = 226, 
+        TTS_PHON_B7 = 227, 
+        TTS_PHON_B8 = 228, 
+        TTS_PHON_B9 = 229, 
+        TTS_PHON_C0 = 230, 
+        TTS_PHON_C1 = 231, 
+        TTS_PHON_C2 = 232, 
+        TTS_PHON_C3 = 233, 
+        TTS_PHON_C4 = 234, 
+        TTS_PHON_C5 = 235, 
+        TTS_PHON_C6 = 236, 
+        TTS_PHON_C7 = 237, 
+        TTS_PHON_C8 = 238, 
+        TTS_PHON_C9 = 239, 
+        TTS_PHON_D0 = 240, 
+        TTS_PHON_D1 = 241, 
+        TTS_PHON_D2 = 242, 
+        TTS_PHON_D3 = 243, 
+        TTS_PHON_D4 = 244, 
+        TTS_PHON_D5 = 245, 
+        TTS_PHON_D6 = 246, 
+        TTS_PHON_D7 = 247, 
+        TTS_PHON_D8 = 248, 
+        TTS_PHON_D9 = 249, 
+        TTS_PHON_D10 = 250, 
+        TTS_PHON_D11 = 251, 
+        TTS_PHON_M0 = 252, 
+        TTS_PHON_M1 = 253, 
+        TTS_PHON_M2 = 254, 
+        TTS_PHON_END = 255
+    };
+    /* The type of a tts code */
+    enum tts_code_type_t : uint8_t {
+        TTS_TYPE_PHONEME,
+        TTS_TYPE_COMMAND,
+        TTS_TYPE_INVALID
+    };
+
+    /* Check if value is a command or a phoneme */
+    tts_code_type_t tts_code_type(const uint8_t& code);
+    /* Go to next value in given range, for a given code */
+    void tts_code_next(uint8_t& code);
+    /* Go to prev value in given range, for a given code */
+    void tts_code_prev(uint8_t& code);
+    /* Return the instruction for a code */
+    const char* tts_code_instruction(const uint8_t& code);
+    /* Return the name of a code */
+    const char* tts_code_name(const uint8_t& code);
+
+    // /* Ensure code_t is a POD & 8-bit wide*/
+    // #ifdef BUILD_CONFIG_DEBUG
+    //     static_assert(std::is_pod<tts_cmd_t>::value, "code_t must be a POD !");
+    //     static_assert (sizeof(tts_cmd_t) == sizeof(uint8_t), "code_t must be 8-bits wide !");
+    // #endif
+};
+
+#endif
