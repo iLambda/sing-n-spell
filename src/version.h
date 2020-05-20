07d3dcdc0fe08d65ab37491e4ce5f27a6391dde6
iLambda <me@ilambda.io>
2020-05-20 23:57:36 +0200

    Build number change

--- src/version.h
+++ src/version.h
@@ -1,16 +1,16 @@
 #ifndef __VERSION_NBUILD
 #define __VERSION_NBUILD
  
-	#define BUILD_N 106 
+	#define BUILD_N 114 
 
 	/* Debug mode */
 	#ifdef BUILD_CONFIG_DEBUG
 		#define BUILD_VERSION "0.0.2D" 
-		#define BUILD_VERSION_NBUILD "0.0.2#106D" 
+		#define BUILD_VERSION_NBUILD "0.0.2#114D" 
 	/* Release mode */
 	#else
 		#define BUILD_VERSION "0.0.2" 
-		#define BUILD_VERSION_NBUILD "0.0.2#106" 
+		#define BUILD_VERSION_NBUILD "0.0.2#114" 
 	#endif
 
 #endif
