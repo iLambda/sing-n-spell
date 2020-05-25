c36822c742507b142007b29cc5523b2d8fcac52c
iLambda <me@ilambda.io>
2020-05-25 04:12:33 +0200

    Added math functions

--- src/utils/math.h
+++ src/utils/math.h
@@ -9,6 +9,16 @@ namespace utils {
     template <class T>
     MBED_FORCEINLINE T mod(const T& a, const T& b) {
         return (a%b+b) % b;
+    } 
+
+    /* Rounding a float to an integer */
+    MBED_FORCEINLINE int round(float f) {
+        return (int)(f + 0.5 - (f < 0));
+    }
+
+    /* Rounding a positive float to an unsigned integer */
+    MBED_FORCEINLINE unsigned int uround(float f) {
+        return (int)(f + 0.5);
     }
 
 }
